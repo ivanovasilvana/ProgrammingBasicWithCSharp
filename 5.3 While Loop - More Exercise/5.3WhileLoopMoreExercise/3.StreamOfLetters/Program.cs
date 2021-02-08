2a9740d973bf1d56f63521f8468690a11ef567ed
ivanovasilvana <71032525+ivanovasilvana@users.noreply.github.com>
2021-02-08 18:46:20 +0200

    While Loop - More Exc

---
+++ 5.3 While Loop - More Exercise/5.3WhileLoopMoreExercise/3.StreamOfLetters/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _3.StreamOfLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string result = "";
+            string print = "";
+
+            bool isCFund = false;
+            bool isOFund = false;
+            bool isNFund = false;
+            bool isFirstFound = false;
+
+            while (input != "End")
+            {
+                char letter = input[0];
+                bool isLetter = char.IsLetter(letter);
+
+                if (!isLetter)
+                {
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                else
+                {
+                    if (letter == 'n')
+                }
+            }
+        }
+    }
+}
