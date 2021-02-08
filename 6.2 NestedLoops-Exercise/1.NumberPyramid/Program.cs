5ff3a1b8f61e001ef1708dd1c00f9aa81af9c2c1
ivanovasilvana <71032525+ivanovasilvana@users.noreply.github.com>
2021-02-08 18:47:35 +0200

    NestedLoops - Exc

---
+++ 6.2 NestedLoops-Exercise/1.NumberPyramid/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _1.NumberPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int row = 1;
+            int currentNumber = 1;
+            bool isEqual = false;
+
+            while (isEqual == false)
+            {
+                for (int i = 1; i <= row; i++)
+                {
+                    Console.Write($"{currentNumber} ");
+                    currentNumber++;
+
+                    if (currentNumber > number)
+                    {
+                        isEqual = true;
+                        break;
+                    }
+                }
+
+                Console.WriteLine();
+                row++;
+            }
+        }
+    }
+}
